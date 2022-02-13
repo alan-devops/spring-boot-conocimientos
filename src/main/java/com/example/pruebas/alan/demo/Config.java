64eb82922f0cf6de4685d8d885361bce9785f79f
Alan <alan.gonzales.jobs@gmail.com>
2022-02-13 19:38:43 +0100

    subida develop pruebaConfig

---
+++ src/main/java/com/example/pruebas/alan/demo/Config.java
@@ -0,0 +1,11 @@
+package com.example.pruebas.alan.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Config {
+
+  public void pintaConfig(){
+    System.out.println("soy el config ten cuidado");
+  }
+}
