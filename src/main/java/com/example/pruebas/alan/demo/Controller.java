1d7bce6c58327385cf7f262c4434458cb1605127
alan-devops <alan.gonzales.jobs@gmail.com>
2022-02-13 19:17:32 +0100

    subida 2 controller

---
+++ src/main/java/com/example/pruebas/alan/demo/Controller.java
@@ -0,0 +1,15 @@
+package com.example.pruebas.alan.demo;
+
+public class Controller {
+
+
+  public void metodo1(){
+    System.out.println("prueba 1");
+  }
+
+  public void metodo2(){
+    System.out.println("prueba 2");
+  }
+
+
+}
