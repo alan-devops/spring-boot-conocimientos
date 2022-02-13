ec18a1e2eba9898d94d0742500dd3eba050412ee
Alan <alan.gonzales.jobs@gmail.com>
2022-02-13 19:24:39 +0100

    prueba 1 develop

--- src/main/java/com/example/pruebas/alan/demo/Controller.java
+++ src/main/java/com/example/pruebas/alan/demo/Controller.java
@@ -3,13 +3,16 @@ package com.example.pruebas.alan.demo;
 public class Controller {
 
 
-  public void metodo1(){
+  public void metodo1() {
     System.out.println("prueba 1");
   }
 
-  public void metodo2(){
+  public void metodo2() {
     System.out.println("prueba 2");
   }
 
+  public void metodo3() {
+    System.out.println("prueba 3");
+  }
 
 }
